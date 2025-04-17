5f85f4f110e57d26d3813081de907822ef6d4abe
bradleycfleming <brad10fleming@gmail.com>
2025-04-17 09:39:36 -0500

    Removed security issue

--- lib/import.py
+++ lib/import.py
@@ -1,15 +1,16 @@
 import pandas as pd
 from supabase import create_client, Client
+import os
+from dotenv import load_dotenv
 
-url = "https://rzthxoaawhileenyobpq.supabase.co"
-key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ6dGh4b2Fhd2hpbGVlbnlvYnBxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQ4NDMyMTQsImV4cCI6MjA2MDQxOTIxNH0.lmB_C8B07bGStpfNsMfwc9xMT6182lI-Uus8Dn-KeV4"  # use service key for insert permissions
+load_dotenv(dotenv_path="../.env.local")
+url = os.getenv("NEXT_PUBLIC_SUPABASE_URL")
+key = os.getenv("NEXT_PUBLIC_SUPABASE_ANON_KEY")
+
 supabase: Client = create_client(url, key)
 
-df = pd.read_csv(r"C:\Users\brad1\Downloads\lichess_db_puzzle.csv")
-
+df = pd.read_csv(r"C:\Users\brad1\Downloads\smallLichessDB.csv")
 df = df.fillna('')
-
-print("hello world")
 
 for row in df.to_dict(orient="records"):
     supabase.table("puzzles").insert(row).execute()
